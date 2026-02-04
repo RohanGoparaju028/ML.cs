c1611b86794e5f4f715e7ee192f0669032ad6baa
Rohan Goparaju <rohangoparaju@Rohans-MacBook-Air.local>
2026-02-04 15:56:15 -0500

    Add MAE, MSE, and R2Score metrics to DataPreprocessing

---
+++ DataPreprocessing/MAE.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.Data.Analysis;
+namespace ML.cs.ModelEvaluation.MAE;
+public class MAE {
+    public  double MAE_Score(PrimitiveDataFrameColumn<double> y_test,PrimitiveDataFrameColumn<double> y_pred) {
+        double sum = 0.0;
+        for(int i=0;i<y_test.Length;i++) {
+            sum += Math.Abs(y_test[i]!.Value - y_pred[i]!.Value);
+        }
+        return sum / (double) y_test.Length;
+    }
+}
---
+++ DataPreprocessing/R2Score.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using Microsoft.Data.Analysis;
+namespace ML.cs.ModelEvaluation.R2;
+public class R2 {
+    public  double R2_Score(PrimitiveDataFrameColumn<double> y_test,PrimitiveDataFrameColumn<double> y_pred) {
+        var y_mean = y_test.Average()!.Value;
+        double ss_res = 0.0d;
+        double ss_total = 0.0;
+        for(int i=0;i<y_test.Length;i++) {
+            double residual_diff = y_test[i]!.Value - y_pred[i]!.Value;
+            double total_diff =  y_test[i]!.Value - y_mean;
+            ss_res += residual_diff * residual_diff;
+            ss_total += total_diff * total_diff;
+        }
+        return 1 - ss_res/ss_total;
+    }
+}
